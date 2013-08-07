8f4ed3128445b0470da3c8ae10b5b0634a83d60e
Ramón Roche <mrpollo@gmail.com>
2013-08-07 11:57:36 -0700

    picking list button on shipment

--- app/overrides/add_picking_list.rb
+++ app/overrides/add_picking_list.rb
@@ -1,5 +1,4 @@
-Deface::Override.new(:virtual_path  => "spree/admin/shipments/edit",
+Deface::Override.new(:virtual_path   => "spree/admin/orders/_shipment",
                      :name          => "add_picking_list_to_shipment_edit",
-                     :insert_top    => "[data-hook='admin_shipment_edit_header'], #admin_shipment_edit_header[data-hook]",
-                     :text          => "<div id='print'> <%= link_to_with_icon 'icon-print', t(:picking_list), admin_order_shipment_list_url(@order, @shipment) %> </div>")
-
+                     :insert_bottom  => "[data-hook='stock-location'], #stock-location[data-hook]",
+                     :text          => "&nbsp;- <%= link_to Spree.t(:picking_list), admin_order_shipments_picking_list_url(order, shipment), {:class => 'ship button icon-print'} %>")
